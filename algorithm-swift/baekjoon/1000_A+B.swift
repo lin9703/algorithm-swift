372bcc1427031329c35d86b79a40eb2836da5e56
Hyorin <hyorin97@naver.com>
2022-03-05 21:21:06 +0900

    백준 1000 A+B

---
+++ algorithm-swift/baekjoon/1000_A+B.swift
@@ -0,0 +1,21 @@
+//
+//  1000_A+B.swift
+//  algorithm-swift
+//
+//  Created by river.o on 2022/03/05.
+//
+
+import Foundation
+
+let input = readLine()!.components(separatedBy: " ")
+
+let first = Int(input[0])!
+let second = Int(input[1])!
+
+let sum = first + second
+print(sum)
+
+
+
+
+
